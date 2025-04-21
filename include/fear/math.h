a9f68181a1879f7692ac88195159e66e1374a13f
destoer <destoer1234@protonmail.com>
2025-04-21 21:23:27 +0100

    Add min functions

--- include/fear/math.h
+++ include/fear/math.h
@@ -2,3 +2,17 @@
 #include <fear/type.h>
 
 s64 fear_abs(s64 v1);
+
+struct Vec2I {
+    s32 x;
+    s32 y;
+};
+
+s32 fear_min2_s32(s32 v1, s32 v2);
+s32 fear_min3_s32(s32 v1, s32 v2, s32 v3);
+
+
+s32 fear_max2_s32(s32 v1, s32 v2);
+s32 fear_max3_s32(s32 v1, s32 v2, s32 v3);
+
+s32 fear_dot_product_2i(struct Vec2I v1, struct Vec2I v2);
--- src/fear/math.c
+++ src/fear/math.c
@@ -3,3 +3,29 @@
 s64 fear_abs(s64 v1) {
     return v1 < 0? -v1 : v1;
 }
+
+s32 fear_min2_s32(s32 v1, s32 v2) {
+    return v1 < v2? v1 : v2;
+}
+
+s32 fear_min3_s32(s32 v1, s32 v2, s32 v3) {
+    s32 min = fear_min2_s32(v1,v2);
+    min = fear_min2_s32(min,v3);
+
+    return min;
+}
+
+s32 fear_max2_s32(s32 v1, s32 v2) {
+    return v1 > v2? v1 : v2;
+}
+
+s32 fear_max3_s32(s32 v1, s32 v2, s32 v3) {
+    s32 max = fear_max2_s32(v1,v2);
+    max = fear_max2_s32(max,v3);
+
+    return max;
+}
+
+s32 fear_dot_product_2i(struct Vec2I v1, struct Vec2I v2) {
+    return (v1.x * v2.x) + (v1.y * v2.y);
+}
